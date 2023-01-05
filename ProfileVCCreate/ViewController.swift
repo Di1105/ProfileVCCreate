8c02f043acae9d8e7eda1180600f33cdd0bce600
Di1105 <delcioglu@gmail.com.com>
2023-01-05 11:04:18 +0300

    Initial Commit

---
+++ ProfileVCCreate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileVCCreate
+//
+//  Created by Dilara Elçioğlu on 5.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
